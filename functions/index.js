7f74624eda2b700e5fa746ec4b479080e242e6bc
Colton Morris <colton.morris@tcnbroadcasting.com>
2019-04-12 15:08:30 -0600

    basic firestore functions

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,45 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as bodyParser from "body-parser";
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
+const bodyParser = require("body-parser")
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp(functions.config().firebase)
 
-const db = admin.firestore();
+const db = admin.firestore()
 
-const app = express();
-const main = express();
+const app = express()
+const main = express()
 
-const lunchesCollection = 'lunches';
+const lunchesCollection = 'lunches'
 
-main.use('/api/v1', app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
+main.use('/api/v1', app)
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({ extended: false }))
 
 // webApi is your functions name, and you will pass main as 
 // a parameter
 // our url will be:
 // 	https://idk.firebaseapp.com/api/v1/**
-export const webApi = functions.https.onRequest(main);
+exports.webApi = functions.https.onRequest(main)
 
 
 // Add new lunch
 app.post('/lunches', (req, res) => {
 	// firebaseHelper.firestore
-	// 	.createNewDocument(db, lunchesCollection, req.body);
-	res.send('Create a new lunch');
+	// 	.createNewDocument(db, lunchesCollection, req.body)
+	res.send('Create a new lunch')
 })
 // Update new lunch
 app.patch('/lunches/:lunchId', (req, res) => {
 	// firebaseHelper.firestore
-	// 	.updateDocument(db, lunchesCollection, req.params.lunchId, req.body);
-	res.send('Update a new lunch');
+	// 	.updateDocument(db, lunchesCollection, req.params.lunchId, req.body)
+	res.send('Update a new lunch')
 })
 // View a lunch
 app.get('/lunches/:lunchId', (req, res) => {
 	// firebaseHelper.firestore
 	// 	.getDocument(db, lunchesCollection, req.params.lunchId)
-		// .then(doc => res.status(200).send(doc));
+		// .then(doc => res.status(200).send(doc))
 })
 
 // View all lunches
@@ -52,6 +52,6 @@ app.get('/lunches', (req, res) => {
 // Delete a lunch 
 app.delete('/lunches/:lunchId', (req, res) => {
 	// firebaseHelper.firestore
-	// 	.deleteDocument(db, lunchesCollection, req.params.lunchId);
-	res.send('Document deleted');
+	// 	.deleteDocument(db, lunchesCollection, req.params.lunchId)
+	res.send('Document deleted')
 })
