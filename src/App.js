a44e458b267cce1aa795ea24bcd7ddc073d0a5c4
Gunnar Gabrielson <gunnar.gabrielson@tcnbroadcasting.com>
2019-04-12 15:38:27 -0600

    added firebase and auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardContent, Grid } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Card, CardContent, Grid, CircularProgress, Typography, Button } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
 
@@ -7,34 +7,51 @@ import NominationRound from './NominationRound';
 import VotingRound from './VotingRound';
 import FinalRound from './FinalRound';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { FirebaseContext } from './Firebase';
+
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <Card elevation={2}>
-            <CardContent>
-              <NominationRound />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Card elevation={2}>
-            <CardContent>
-              <VotingRound />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Card elevation={2}>
-            <CardContent>
-              <FinalRound />
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+  const firebase = useContext(FirebaseContext);
+  console.log('firebase', firebase);
+  const {initialising, user} = useAuthState(firebase.auth);
+
+
+  if (initialising) return <CircularProgress />;
+  if (!user) return (
+    <Typography>
+      You Must <Button variant="contained" color="primary" onClick={firebase.login}>Login</Button>
+    </Typography>
   );
+
+  return <Button onClick={firebase.logout}>Logout</Button>;
+
+  // return (
+  //   <ThemeProvider theme={theme}>
+  //     <Grid container spacing={2}>
+  //       <Grid item xs={12} sm={4}>
+  //         <Card elevation={2}>
+  //           <CardContent>
+  //             <NominationRound />
+  //           </CardContent>
+  //         </Card>
+  //       </Grid>
+  //       <Grid item xs={12} sm={4}>
+  //         <Card elevation={2}>
+  //           <CardContent>
+  //             <VotingRound />
+  //           </CardContent>
+  //         </Card>
+  //       </Grid>
+  //       <Grid item xs={12} sm={4}>
+  //         <Card elevation={2}>
+  //           <CardContent>
+  //             <FinalRound />
+  //           </CardContent>
+  //         </Card>
+  //       </Grid>
+  //     </Grid>
+  //   </ThemeProvider>
+  // );
 };
 
 export default App;
---
+++ src/Firebase/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import * as app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const config = {
+  apiKey: "AIzaSyCItLM5rJ0BW2h8j3pDthJBGcgPQ6tyfps",
+  authDomain: "lunchyclub.firebaseapp.com",
+  databaseURL: "https://lunchyclub.firebaseio.com",
+  projectId: "lunchyclub",
+  storageBucket: "lunchyclub.appspot.com",
+  messagingSenderId: "341779644521"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+  }
+
+  login = () => {
+    const provider = new app.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    this.auth.signInWithRedirect(provider);
+  };
+
+  logout = () => {
+    this.auth.signOut();
+  }
+
+}
+
+export default Firebase;
+
+
+const FirebaseContext = React.createContext(null);
+export { FirebaseContext };
