4ac27aaec951e2ef53fcf7a454a0a938d0b2b3f2
Colton Morris <colton.morris@tcnbroadcasting.com>
2019-04-12 13:09:07 -0600

    createRound1

---
+++ src/data_structure_utils/README.md
@@ -0,0 +1,6 @@
+# Backend Info
+
+[github](https://github.com/tcn-tgif/lunchy)
+[firestore](https://console.firebase.google.com/project/lunchyclub/overview)
+
+
---
+++ src/data_structure_utils/data_structures.test.js
@@ -0,0 +1,9 @@
+import createRound1 from './data_structures'
+
+
+it('creates round 1', () => {
+	console.log('hi')
+	let data = createRound1({}, Date.now())
+
+	console.log(data)
+})
