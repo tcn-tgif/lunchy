63972a6d84d0b6bbf4e76858c82f6c459a8043d2
Gunnar Gabrielson <gunnar.gabrielson@tcnbroadcasting.com>
2019-04-12 14:33:34 -0600

    grid

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    height: "100vh",
-    backgroundColor: "grey",
-  },
-  paper: {
-    minHeight: "100px",
-    width: "25vw",
-  },
-})
+// const useStyles = makeStyles({
+//   root: {
+//     flexGrow: 1,
+//     height: "100vh",
+//     backgroundColor: "grey",
+//   },
+//   paper: {
+//     minHeight: "100px",
+//     width: "25vw",
+//   },
+// })
 
 
 const App = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
+
+  const card = (
+    <Card elevation={2}>
+      <CardContent>
+        <Typography>Column</Typography>
+      </CardContent>
+    </Card>
+  );
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={12}>
-        <Grid container justify="center" alignItems="stretch" direction="row">
-          {[0,1,2].map(value => (
-            <Grid key={value} item>
-              <Paper className={classes.paper} />
-            </Grid>
-          ))}
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          {card}
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          {card}
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          {card}
         </Grid>
       </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 };
 
