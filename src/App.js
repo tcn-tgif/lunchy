53a113e4bfe03d89b44f01dc990cb6054e76e398
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2019-04-12 14:18:14 -0600

    oops

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    height: "100vh",
+    backgroundColor: "grey",
+  },
+  paper: {
+    minHeight: "100px",
+    width: "25vw",
+  },
+})
+
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Grid container justify="center" alignItems="stretch" direction="row">
+          {[0,1,2].map(value => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default App;
